f5cd0eaf2768743b056f8d0bbb7b656cc272bfab
Valery Bogdanov <valery.bogdanov@ideus.biz>
2016-05-04 19:53:11 +0300

    Revert "Add jQuery"
    This reverts commit 0b31f700c0dbadd020267cdb4050e90355dff598.

--- inc/footer.php
+++ inc/footer.php
@@ -5,9 +5,6 @@
 
 
   <!-- JavaScript -->
-  <script src="//ajax.googleapis.com/ajax/libs/jquery/2.2.3/jquery.min.js"></script>
-  <script>window.jQuery || document.write('<script src="js/vendor/jquery-2.2.3.min.js"><\/script>')</script>
-
   <script src="js/main.js"></script>
 
   <?php foreach($js as $value): ?>
