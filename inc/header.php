ffe031a01586f714f8e0cc0a00122459793c0c52
Valery Bogdanov <valery.bogdanov@ideus.biz>
2016-05-04 19:04:07 +0300

    Fix Google Fonts

--- inc/header.php
+++ inc/header.php
@@ -8,9 +8,7 @@
   <title>R&D</title>
 
   <!-- Google Fonts -->
-  <link href='https://fonts.googleapis.com/css?family=Roboto:400,300italic,300,400italic,500,500italic,700,700italic,100italic,100&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
-  <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,300,700,100&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
-  <link href='https://fonts.googleapis.com/css?family=Inconsolata:400,700' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:100,100italic,300,300italic,400,400italic,500,500italic,700,700italic|Roboto+Slab:100,300,400,700|Inconsolata:400,700" />
 
   <!-- CSS -->
   <link rel="stylesheet" href="css/main.css">
