0b31f700c0dbadd020267cdb4050e90355dff598
Valery Bogdanov <valery.bogdanov@ideus.biz>
2016-05-04 19:43:12 +0300

    Add jQuery

--- inc/footer.php
+++ inc/footer.php
@@ -5,6 +5,9 @@
 
 
   <!-- JavaScript -->
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/2.2.3/jquery.min.js"></script>
+  <script>window.jQuery || document.write('<script src="js/vendor/jquery-2.2.3.min.js"><\/script>')</script>
+
   <script src="js/main.js"></script>
 
   <?php foreach($js as $value): ?>
